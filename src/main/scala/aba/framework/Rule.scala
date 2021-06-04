414e74553496a7844f1ba3313c8914d895cd3fc2
gorczyca <gorczycapj@gmail.com>
2021-06-04 17:59:47 +0200

    Changed unicode arrow to avoid problems with windows terminal

--- src/main/scala/aba/framework/Rule.scala
+++ src/main/scala/aba/framework/Rule.scala
@@ -6,5 +6,6 @@ case class Rule(id: Option[String], head: Literal, body: Set[Literal]) {
     this(None, head, body)
   }
 
-  override def toString: String = s"$head ← ${body.mkString(",")}"
+  // <- ←
+  override def toString: String = s"$head <- ${body.mkString(",")}"
 }
