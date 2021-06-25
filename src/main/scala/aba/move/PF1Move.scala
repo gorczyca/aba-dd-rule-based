a523c3a17f10bd0d8bae9ed5171447ac40970810
gorczyca <gorczycapj@gmail.com>
2021-06-25 12:21:30 +0200

    Corrected PF1 error (heads of unexpanded prop. statements)

--- src/main/scala/aba/move/PF1Move.scala
+++ src/main/scala/aba/move/PF1Move.scala
@@ -6,8 +6,11 @@ import aba.reasoner.{DisputeState, LiteralArgument, PotentialMove, RuleArgument}
 object PF1Move extends Move {
   override def isPossible(implicit framework: Framework, dState: DisputeState): Seq[PotentialMove] = {
     val completePiecesPLit = framework.completePiecesP.collect { case litArg: LiteralArgument => litArg.lit }
+    val unexpandedPStatementsLit = framework.unexpandedPStatements.collect { case litArg: LiteralArgument => litArg }.map(_.lit)
     framework.remainingNonBlockedPRules
-      .filter( rule => !dState.pLitArgs.map(_.lit).contains(rule.head) && rule.body.subsetOf(completePiecesPLit)).map(RuleArgument) // definition
+      .filter( rule => (!dState.pLitArgs.map(_.lit).contains(rule.head) || unexpandedPStatementsLit.contains(rule.head) )
+        && rule.body.subsetOf(completePiecesPLit))
+      .map(RuleArgument) // definition
       .diff(dState.pRuleArgs) // prevent from repeating
       .toSeq.sortBy(_.rule.head.id) // sorting
       .map( ruleArg => PotentialMove(Some(ruleArg), None, Set(LiteralArgument(ruleArg.rule.head)), Move.PF1, None) )
