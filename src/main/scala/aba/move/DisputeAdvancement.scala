d981bf15a62cb6774ec52f541256ad124384d452
gorczyca <gorczycapj@gmail.com>
2021-06-30 18:56:36 +0200

    Implemented DisputeAdvancement types and TerminationCriteria types

---
+++ src/main/scala/aba/move/DisputeAdvancement.scala
@@ -0,0 +1,83 @@
+package aba.move
+
+import aba.framework.{Framework, Literal}
+import aba.move.DisputeAdvancement.DisputeAdvancementType
+import aba.move.Move.{MoveType, OB1, OB2, OF1, OF2, PB1, PB2, PF1, PF2}
+import aba.reasoner.{DisputeState, PotentialMove}
+
+object DisputeAdvancement extends Enumeration {
+  type DisputeAdvancementType = Value
+
+  val
+    DF,
+    DAB,
+    DABF,
+    DC,
+    DS
+  = Value
+
+  implicit def fromString(advancementTypeString: String): DisputeAdvancementType = values.find(_.toString.equalsIgnoreCase(advancementTypeString)) match {
+    case Some(value) => value
+    case None => throw new Exception(s"No advancement type: $advancementTypeString")
+  }
+
+  def apply(advancementType: DisputeAdvancementType)(implicit framework: Framework, dState: DisputeState): DisputeAdvancement = {
+    val moves = advancementType match {
+      case DF => Seq(
+        (PB1, framework.contrariesOf(framework.assumptions)),
+        (PB2, framework.contrariesOf(framework.assumptions)),
+        (PF1, framework.assumptions),
+        (PF2, framework.assumptions),
+        (OB1, framework.contrariesOf(framework.assumptions)),
+        (OB2, framework.contrariesOf(framework.assumptions)),
+        (OF1, framework.assumptions),
+        (OF2, framework.assumptions),
+      )
+      case DAB => Seq(
+        (PB1, Set.empty[Literal]),
+        (PB2, Set.empty[Literal]),
+        (PF2, Set.empty[Literal]),
+        (OB1, Set.empty[Literal]),
+        (OB2, Set.empty[Literal]),
+        (OF2, Set.empty[Literal]),
+      )
+      case DABF => Seq(
+        (PB1, Set.empty[Literal]),
+        (PB2, Set.empty[Literal]),
+        (PF1, Set.empty[Literal]),
+        (PF2, Set.empty[Literal]),
+        (OB1, Set.empty[Literal]),
+        (OB2, Set.empty[Literal]),
+        (OF2, Set.empty[Literal]),
+      )
+      case DC => Seq(
+        (PB1, Set.empty[Literal]),
+        (PB2, Set.empty[Literal]),
+        (PF1, framework.j),
+        (PF2, framework.j),
+        (OB1, framework.contrariesOf(framework.j)),
+        (OB2, framework.contrariesOf(framework.j)),
+        (OF2, Set.empty[Literal]),
+      )
+      case DS => Seq(
+        (PB1, framework.contrariesOf(framework.assumptions)),
+        (PB2, framework.contrariesOf(framework.assumptions)),
+        (PF1, framework.assumptions),
+        (PF2, framework.assumptions),
+        (OB1, Set.empty[Literal]),
+        (OB2, Set.empty[Literal]),
+        (OF2, Set.empty[Literal]),
+      )
+    }
+    DisputeAdvancement(advancementType, moves)
+  }
+}
+
+
+case class DisputeAdvancement(advancementType: DisputeAdvancementType,
+                              moves: Seq[(MoveType, Set[Literal])]) {
+
+  def getPossibleMoves(implicit framework: Framework, dState: DisputeState): Map[MoveType, Seq[PotentialMove]] = {
+    moves.map { case (moveType, set) => (moveType, Move(moveType).isPossible(set)) }.filter(_._2.nonEmpty).toMap
+  }
+}
---
+++ src/main/scala/aba/move/TerminationCriteria.scala
@@ -0,0 +1,102 @@
+package aba.move
+
+import aba.framework.Framework
+import aba.reasoner.{DisputeState, LiteralArgument}
+import aba.move.DisputeAdvancement.{DAB, DC, DF, DS}
+
+object TerminationCriteria extends Enumeration {
+  type TerminationCriteriaType = Value
+
+  val
+   TA,
+   TC,
+   TS
+  = Value
+
+  implicit def fromString(terminationTypeString: String): TerminationCriteriaType = values.find(_.toString.equalsIgnoreCase(terminationTypeString)) match {
+    case Some(value) => value
+    case None => throw new Exception(s"No termination criteria type: $terminationTypeString")
+  }
+
+  def proponentSeemsToBeWinning(implicit framework: Framework, dState: DisputeState): Boolean = {
+    val unblockedCompletePlayedPiecesLit = framework.unblockedCompletePlayedPiecesB.collect { case litArg: LiteralArgument => litArg.lit }
+    val completeArgsPLit = framework.completePiecesP.collect { case litArg: LiteralArgument => litArg.lit }
+
+    val condition1 = (framework.contrariesOf(framework.defences) intersect unblockedCompletePlayedPiecesLit).isEmpty
+    val condition2 = (framework.goals union framework.contrariesOf(framework.culprits)).subsetOf(completeArgsPLit)
+
+    condition1 && condition2
+  }
+
+  def opponentSeemsToBeWinning(implicit framework: Framework, dState: DisputeState): Boolean = !proponentSeemsToBeWinning
+
+  def checkIfOver(terminationCriteriaType: TerminationCriteriaType)(implicit framework: Framework, dState: DisputeState): Option[Boolean] = {
+
+//    val (dabOpponentMoves, dabProponentMoves) = DisputeAdvancement(DAB).getPossibleMoves.partition(_._1.isOpponentsMove)
+//    val (dfOpponentMoves, dfProponentMoves) = DisputeAdvancement(DF).getPossibleMoves.partition(_._1.isOpponentsMove)
+//    val (dcOpponentMoves, dcProponentMoves) = DisputeAdvancement(DC).getPossibleMoves.partition(_._1.isOpponentsMove)
+//    val (_, dsProponentMoves) = DisputeAdvancement(DS).getPossibleMoves.partition(_._1.isOpponentsMove)
+
+    val Seq((dabOpponentMoves, dabProponentMoves),
+            (dfOpponentMoves, dfProponentMoves),
+            (dcOpponentMoves, dcProponentMoves),
+            (_, dsProponentMoves))
+      = Seq(DisputeAdvancement(DAB),
+            DisputeAdvancement(DF),
+            DisputeAdvancement(DC),
+            DisputeAdvancement(DS))
+      .map(_.getPossibleMoves.partition(_._1.isOpponentsMove))
+
+
+    val (propCondBool, oppCondBool) = terminationCriteriaType match {
+      case TA => (
+        // prop condition
+        proponentSeemsToBeWinning &&
+          (dabOpponentMoves.forall(_._2.isEmpty) || dfOpponentMoves.filter(_._1.isForwardMove).forall(_._2.isEmpty)),
+        // opp condition
+        opponentSeemsToBeWinning &&
+          (dabProponentMoves.forall(_._2.isEmpty) || dfProponentMoves.filter(_._1.isForwardMove).forall(_._2.isEmpty))
+      )
+      case TC => (
+        // prop condition
+        proponentSeemsToBeWinning && (framework.j -- framework.defences).isEmpty &&
+           (dcOpponentMoves.forall(_._2.isEmpty) || dfOpponentMoves.filter(_._1.isForwardMove).forall(_._2.isEmpty)),
+        // opp condition
+        (opponentSeemsToBeWinning || (framework.j -- framework.defences).nonEmpty) &&
+          (dcProponentMoves.forall(_._2.isEmpty) || dfProponentMoves.filter(_._1.isForwardMove).forall(_._2.isEmpty) )
+      )
+      case TS => (
+        // prop condition
+        proponentSeemsToBeWinning && ((framework.defences ++ framework.culprits) == framework.assumptions) &&
+          (dabOpponentMoves.forall(_._2.isEmpty) || dfOpponentMoves.filter(_._1.isForwardMove).forall(_._2.isEmpty)),
+        // opp condition
+        (opponentSeemsToBeWinning || ((framework.defences ++ framework.culprits) != framework.assumptions)) &&
+          dsProponentMoves.forall(_._2.isEmpty)
+      )
+    }
+
+    if(propCondBool) Some(true)
+    else if (oppCondBool) Some(false)
+    else None
+  }
+
+
+  def seemsToBeWinningDebug(implicit dState: DisputeState, framework: Framework): Unit = {
+
+    val unblockedCompletePlayedPiecesLit = framework.unblockedCompletePlayedPiecesB.collect { case litArg: LiteralArgument => litArg.lit }
+    val completeArgsPLit = framework.completePiecesP.collect { case litArg: LiteralArgument => litArg.lit }
+
+    val defencesContraries = framework.contrariesOf(framework.defences)
+    val condition1 = (framework.contrariesOf(framework.defences) intersect unblockedCompletePlayedPiecesLit).isEmpty
+    println(s"Condition 1: ${if (condition1) "TRUE" else "FALSE" }")
+    println(s"(DefencesContraries INTERSECT UnblockedCompletePlayedPieces) is empty?")
+    println(s"(${defencesContraries.mkString(", ")} INTERSECT ${unblockedCompletePlayedPiecesLit.mkString(", ")}) is empty?")
+
+
+    val culpritContraries = framework.contrariesOf(framework.culprits)
+    val condition2 = (framework.goals union culpritContraries).subsetOf(completeArgsPLit)
+    println(s"Condition 2: ${if (condition2) "TRUE" else "FALSE" }")
+    println(s"((Goals UNION CulpritContraries) SUBSETOF CompletePArgs)")
+    println(s"(${framework.goals.mkString(", ")} UNION ${culpritContraries.mkString(", ")}) SUBSETOF ${completeArgsPLit.mkString(", ")})")
+  }
+}
