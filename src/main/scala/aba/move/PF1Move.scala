3f0dceb8762becdcb983254129c697ee4aa913cb
gorczyca <gorczycapj@gmail.com>
2021-07-02 15:15:14 +0200

    Modified PF1 move requirements to match current version

--- src/main/scala/aba/move/PF1Move.scala
+++ src/main/scala/aba/move/PF1Move.scala
@@ -8,7 +8,8 @@ object PF1Move extends Move {
     val completePiecesPLit = framework.completePiecesP.collect { case litArg: LiteralArgument => litArg.lit }
 
     framework.remainingNonBlockedPRules
-      .filter( rule => !dState.pLitArgs.map(_.lit).contains(rule.head) && rule.body.subsetOf(completePiecesPLit) ) // definition
+      .filter( rule => (!dState.pLitArgs.map(_.lit).contains(rule.head) || framework.unexpandedPStatements.contains(rule.head))
+        && rule.body.subsetOf(completePiecesPLit) ) // definition
       .map(RuleArgument)
       .diff(dState.pRuleArgs) // prevent from repeating
       .toSeq.sortBy(_.rule.head.id) // sorting
