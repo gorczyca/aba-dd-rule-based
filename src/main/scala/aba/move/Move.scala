11f24261cfc84ca6f1143eea331e88a529ef88b4
gorczyca <gorczycapj@gmail.com>
2021-07-05 18:14:28 +0200

    Added functionality to print all possible moves

--- src/main/scala/aba/move/Move.scala
+++ src/main/scala/aba/move/Move.scala
@@ -2,6 +2,7 @@ package aba.move
 
 import scala.language.implicitConversions
 import aba.framework.{Framework, Literal}
+import aba.move.DisputeAdvancement.{DAB, DABF, DC, DF, DS, DisputeAdvancementType}
 import aba.reasoner.{DisputeState, PotentialMove}
 
 import scala.math.Ordering.Implicits.seqOrdering
@@ -53,6 +54,29 @@ object Move extends Enumeration {
       }.mkString("\n")
     }.mkString("\n")
   }
+
+  def possibleMovesAccordingToAllAdvancementToString(implicit dState: DisputeState, framework: Framework): String = {
+    val advancementTypesMoves = DisputeAdvancement.values
+      .toSeq.sortWith(_ < _)
+      .map(aType => (aType, DisputeAdvancement(aType).getPossibleMoves))
+
+    val moveTypes = advancementTypesMoves.flatMap(_._2.keys).distinct.sortWith(_ < _)
+
+    val x = moveTypes.map(mType => {
+      val innerMap = advancementTypesMoves.filter(_._2.keys.toSeq.contains(mType))
+        .map { case (advType: DisputeAdvancementType, map: Map[MoveType, Seq[PotentialMove]]) =>
+          (advType, map(mType))
+        }.sortWith(_._1 < _._1).toMap
+      (mType, innerMap)
+    })
+
+    x.map { case (mType: MoveType, map: Map[DisputeAdvancementType, Seq[PotentialMove]] ) =>
+      s"$mType:\n" + map.map {
+        { case (dType, potentialMoves) => s"\t$dType:\n" + potentialMoves.map(pMove => s"\t\t$pMove").mkString("\n") }   // TODO: zip with index?
+      }.mkString("\n")
+    }.mkString("\n")
+  }
+
 }
 
 // Abstract class for all moves
