339457af2eeeb7ef46acbfbfd1b5bc1c00a81982
gorczyca <gorczycapj@gmail.com>
2022-12-05 12:14:19 +0100

    Added comments to AbaParser and ApxParser

--- src/main/scala/aba/fileParser/AbaParser.scala
+++ src/main/scala/aba/fileParser/AbaParser.scala
@@ -2,15 +2,94 @@ package aba.fileParser
 
 import aba.framework.{Contrary, Rule}
 
+/** Parser for *.aba files.
+ *
+ * Parses the framework in the following form:
+ *
+ * % Rule
+ * [ID:]HEAD:-BODY.
+ *  with ID being an optional literal, HEAD a literal and BODY a set thereof.
+ *
+ * % Contrary
+ * ASSUMPTION:CONTRARY.
+ *  with ASSUMPTION and CONTRARY being literals.
+ *
+ * % Assumption
+ * ASSUMPTION.
+ *  with assumption being a literal.
+ *
+ * % Goal
+ * ?GOAL.
+ *  with GOAL being a literal.
+ *
+ * % Constraint
+ * !CONSTRAINT.
+ *  with CONSTRAINT being a literal.
+ *
+ */
 object AbaParser extends FileParser {
 
+    /** Parses a rule.
+     *
+     * [ID:]HEAD:-BODY.
+     * with ID being an optional literal, HEAD a literal and BODY a set thereof.
+     *
+     * E.g.
+     *  rule1:h1:-a1,b2,c3.
+     *  h1:-a1,b2,c3.
+     *
+     *  @return Rule parser.
+     */
     override def rule: Parser[Rule] = opt(identifier <~ ": ") ~ identifier~":-"~fields~"." ^^ {
         case Some(rule_id)~head~":-"~body~"." => Rule(Some(rule_id), head, body)
         case None ~ head~":-"~body~"." => Rule(None, head, body)
     }
+
+    /** Parses a contrary.
+     *
+     * ASSUMPTION:CONTRARY.
+     * with ASSUMPTION and CONTRARY being literals.
+     *
+     * E.g.
+     *  a1:x2.
+     *
+     *  @return Contrary parser.
+     */
     override def contrary: Parser[Contrary] = identifier~":"~identifier~"." ^^ { case ass~":"~ctr~"." => Contrary(ass, ctr) }
+
+    /** Parses an assumption.
+     *
+     * ASSUMPTION.
+     * with ASSUMPTION being a literal.
+     *
+     * E.g.
+     *  a1.
+     *
+     *  @return Assumption string parser.
+     */
     override def assumption: Parser[String] = identifier~"." ^^ { case ass~"." => ass }
+
+    /** Parses a goal.
+     *
+     * ?GOAL.
+     * with GOAL being a literal.
+     *
+     * E.g.
+     *  ?h1.
+     *
+     *  @return Goal string parser.
+     */
     override def goal: Parser[String] = "?"~identifier~"." ^^ { case "?"~goal~"." => goal }
+
+    /** Parses a constraint.
+     *
+     * !CONSTRAINT.
+     * with CONSTRAINT being a literal.
+     *
+     * !a2.
+     *
+     *  @return Constraint string parser.
+     */
     override def constraint: Parser[String] = "!"~identifier~"." ^^ { case "!"~con~"." => con }
 
 }
--- src/main/scala/aba/fileParser/ApxParser.scala
+++ src/main/scala/aba/fileParser/ApxParser.scala
@@ -2,13 +2,85 @@ package aba.fileParser
 
 import aba.framework.{ Contrary, Rule }
 
-
+/** Parser for *.apx files.
+ *
+ * Parses the framework in the following form:
+ *
+ * rule(HEAD, [BODY]).
+ *  with HEAD being a single literal and BODY a set thereof.
+ *
+ * contrary(ASSUMPTION, CONTRARY).
+ *  with ASSUMPTION, CONTRARY being literals.
+ *
+ * assumption(ASSUMPTION).
+ *  with ASSUMPTION being a literal.
+ *
+ * goal(GOAL).
+ *  with GOAL being a literal.
+ *
+ * constraint(CONSTRAINT)
+ *  with CONSTRAINT being a literal.
+ */
 object ApxParser extends FileParser {
 
+  /** Parses a rule.
+   *
+   * rule(HEAD, [BODY]).
+   * with HEAD being a single literal and BODY a set thereof.
+   *
+   * E.g.
+   *  rule(h1,[a1,b2,c3]).
+   *
+   *  @return Rule parser.
+   */
   override def rule: Parser[Rule] = "rule("~identifier~",["~fields~"])." ^^ { case "rule("~head~",["~body~"])." => new Rule(head, body)  }
+
+  /** Parses a contrary.
+   *
+   * contrary(ASSUMPTION, CONTRARY).
+   * with ASSUMPTION, CONTRARY being literals.
+   *
+   * E.g.
+   *  contrary(a1,x2).
+   *
+   *  @return Contrary parser.
+   */
   override def contrary: Parser[Contrary] = "contrary("~identifier~","~identifier~")." ^^ { case "contrary("~ass~","~ctr~")." => Contrary(ass, ctr) }
+
+  /** Parses an assumption.
+   *
+   * assumption(ASSUMPTION).
+   * with ASSUMPTION being a literal.
+   *
+   * E.g.
+   *  assumption(a1).
+   *
+   *  @return Assumption string parser.
+   */
   override def assumption: Parser[String] = "asm("~identifier~")." ^^ { case "asm("~ass~")." => ass }
+
+  /** Parses a goal.
+   *
+   * goal(GOAL).
+   * with GOAL being a literal.
+   *
+   * E.g.
+   *  goal(h1).
+   *
+   *  @return Goal string parser.
+   */
   override def goal: Parser[String] = "goal("~identifier~")." ^^ { case "goal("~goal~")." => goal }
+
+  /** Parses a constraint.
+   *
+   * constraint(CONSTRAINT)
+   * with CONSTRAINT being a literal.
+   *
+   * E.g.
+   *  constraint(a2).
+   *
+   *  @return Constraint string parser.
+   */
   override def constraint: Parser[String] = "constraint("~identifier~")." ^^ { case "constraint("~con~")." => con }
 
 
